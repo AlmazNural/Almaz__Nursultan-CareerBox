fe4de8ef3a992fb25f64531b5454e7a90a253c31
ALMAZ <almaz.nursultan@bk.ru>
2023-05-20 22:50:27 +0600

    <commit message>

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+      
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 1500,
+        autoplaySpeed: 1000/true,
+    });
+  });
